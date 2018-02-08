a07397243897ab0173036ed2bea8e05acea86d59
Ashok Palla <ashok_palla@yahoo.com>
2018-02-08 12:01:33 +0530

    body-parser added

--- node/server.js
+++ node/server.js
@@ -1,10 +1,23 @@
 var express = require('express');
-var log = require('./logger/log')
-var mssql = require('./business_layer/mssql');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var log = require('./logger/log');
+var mssql = require('./databse_access_layer/mssql');
 var PORT = 3000;
 
-log.errorLog('this is first log.');
+//The function is executed every time the app receives a request
+app.use(function (req, res, next) {
+    log.errorLog('Time:' + Date.now());
+    next();
+});
 
-app.listen(PORT, () => { console.log('listening port ' + PORT) });
+app.post('/asd', function (req, res) {
+    res.json({ 'name': 'Ashok Palla' });
+});
 
+app.listen(PORT, () => {
+    console.log('listening port ' + PORT)
+});
