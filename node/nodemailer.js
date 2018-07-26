68f4899945ab0d4cec5bf518b9f1d63fc476ace4
Ashok Palla <ashok-palla@users.noreply.github.com>
2018-07-26 15:00:24 +0530

    Update nodemailer.js

--- node/nodemailer.js
+++ node/nodemailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const log = require('./logger/log');
 const fs = require('fs');
 
-const account = { "user": "ashok_palla@merilytics.com", "pass": "G4jrEPqzxv1" };
+const account = { "user": "ashok_palla@merilytics.com", "pass": "" };
 
 let transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
